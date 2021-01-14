2a79590efc2a1470ecd1c7551456fb91fc500766
Eugene Kim <conankunioi@gmail.com>
2021-01-13 19:35:57 -0800

    Add dockerfiles for building rootfs

---
+++ rootfs/generate_images.sh
@@ -0,0 +1,6 @@
+UBUNTU_VERSION=18.04
+GCC_IMAGE_VERSION=1.0
+PY_IMAGE_VERSION=1.0
+sudo docker build --tag codu-online-judge-base:${UBUNTU_VERSION} .
+sudo docker build --tag codu-online-judge-gcc:${GCC_IMAGE_VERSION} -f gcc/Dockerfile .
+sudo docker build --tag codu-online-judge-py:${PY_IMAGE_VERSION} -f py/Dockerfile .
