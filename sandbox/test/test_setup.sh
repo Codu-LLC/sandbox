fd39b269296b83a7a0d00b573aeb850d856cb2b3
Eugene Kim <conankunioi@gmail.com>
2021-01-18 23:47:34 -0800

    fix breakage

--- sandbox/test/test_setup.sh
+++
@@ -1,4 +0,0 @@
-export GROUP=${USER}
-sudo cgcreate -a ${USER}:${GROUP} -g cpu,memory:/test_sandbox
-sudo chown ${USER}:${GROUP} /sys/fs/cgroup/memory/test_sandbox/tasks
-sudo chown ${USER}:${GROUP} /sys/fs/cgroup/cpu/test_sandbox/tasks
