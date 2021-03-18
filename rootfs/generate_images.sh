1335d9cc87e960e98fdfe897d3ce7e335e52aac7
Eugene Kim <conankunioi@gmail.com>
2021-03-18 03:22:05 -0700

    Modify to fit local sandbox

--- rootfs/generate_images.sh
+++ rootfs/generate_images.sh
@@ -5,8 +5,8 @@ PY_IMAGE_VERSION=1.0
 JAVA_IMAGE_VERSION=1.0
 NODE_IMAGE_VERSION=1.0
 
-docker build --tag codu-online-judge-base:${UBUNTU_VERSION} .
-docker build --tag codu-online-judge-gcc:${GCC_IMAGE_VERSION} -f gcc/Dockerfile .
-docker build --tag codu-online-judge-py:${PY_IMAGE_VERSION} -f py/Dockerfile .
-docker build --tag codu-online-judge-java:${JAVA_IMAGE_VERSION} -f java/Dockerfile .
-docker build --tag codu-online-judge-node:${NODE_IMAGE_VERSION} -f node/Dockerfile .
+sudo docker build --tag codu-online-judge-base:${UBUNTU_VERSION} .
+sudo docker build --tag codu-online-judge-gcc:${GCC_IMAGE_VERSION} -f gcc/Dockerfile .
+sudo docker build --tag codu-online-judge-py:${PY_IMAGE_VERSION} -f py/Dockerfile .
+sudo docker build --tag codu-online-judge-java:${JAVA_IMAGE_VERSION} -f java/Dockerfile .
+sudo docker build --tag codu-online-judge-node:${NODE_IMAGE_VERSION} -f node/Dockerfile .
